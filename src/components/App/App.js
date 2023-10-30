7e136411c2396d6016ec0394415ac2f8187524b9
Jen Ziel <jenziel400@gmail.com>
2023-10-30 12:35:56 -0400

    Add working GET request

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,68 @@
-import './App.css';
-import React, {useState} from 'react';
-import Tricks from '../Tricks/Tricks'
-import Form from '../Form/Form'
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import TrickContainer from "../TrickContainer/TrickContainer";
+import Form from "../Form/Form";
 
 function App() {
-  const [tricks, setTricks] = useState([])
+  const dummyTricks = [
+    {
+      stance: "regular",
+      name: "treflip",
+      obstacle: "flat ground",
+      tutorial: "https://www.youtube.com/watch?v=XGw3YkQmNig",
+      id: 1,
+    },
+    {
+      stance: "switch",
+      name: "heelflip",
+      obstacle: "stairs",
+      tutorial: "https://www.youtube.com/watch?v=9N9swrZU1HA",
+      id: 2,
+    },
+    {
+      stance: "regular",
+      name: "frontside 50-50, backside 180 out",
+      obstacle: "ledge",
+      tutorial: "https://www.youtube.com/watch?v=9N9swrZU1HA",
+      id: 3,
+    },
+  ];
+  const [tricks, setTricks] = useState(dummyTricks);
 
   function addTrick(newTrick) {
     setTricks([...tricks, newTrick]);
   }
 
+  const getTricks = () => {
+    return fetch("http://localhost:3001/api/v1/tricks").then((response) => {
+      if (!response.ok) {
+        throw new Error("Oh no! Something went wrong.");
+      }
+      return response.json();
+    });
+  };
+  
+  function displayTricks() {
+    getTricks()
+    .then((data) => {
+      console.log(data);
+      setTricks(data);
+    })
+    .then(()=> console.log(tricks, "tricks"))
+    .catch((error) => console.log("error message:", error.message));
+  }
+
+  useEffect(() => {
+    displayTricks();
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Sick Trick Wish List</h1>
-      < Form addTrick={addTrick}/>
-      <Tricks tricks={tricks}/>
+      <Form addTrick={addTrick} />
+      <TrickContainer tricks={tricks} />
     </div>
   );
 }
 
-export default App; 
+export default App;
