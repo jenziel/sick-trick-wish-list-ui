c61de77020bfd85186bf41eab488c26e06b9c68a
Jen Ziel <jenziel400@gmail.com>
2023-10-30 13:17:42 -0400

    Fix the fixture file so that it stores an array.  Add beforeEach statement to stub data.

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,20 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// Write tests covering what should be displayed on the page when the user first visits.
+
+// Write a test that checks that when data is put into the form, the value is reflected in that form input.
+
+// Write a test to check the user flow of adding a new trick to the DOM.
+
+describe('On pageload I should see a title, a form with 4 input fields and submit button to add a new trick.  I should see trick cards describing tricks. ', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:3001/api/v1/tricks', {
+      statusCode: 200,
+      fixture: 'tricks.json'
+    }).as('getTricks')
+    cy.visit('http://localhost:3000')
+  });
+  
+  it('should include all the info shown on the spec.', () => {
+    cy.wait('@getTricks')
+    cy.get('h1').contains('Sick Trick Wish List')
   })
 })
--- src/components/TrickContainer/TrickContainer.js
+++ src/components/TrickContainer/TrickContainer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Card from '../Card/Card'
 import './TrickContainer.css'
 function TrickContainer({tricks}) {
+
     const trickCards = tricks.map(trick => {
         return (
             <Card   
@@ -15,9 +16,11 @@ function TrickContainer({tricks}) {
         )
     })
   return (
+    <div>
        <div className='trick-container'>
       {trickCards}
         </div>
+    </div>
   )
 }
 
